b61d815196009cf1c4caf3314800344de5afbfc6
ACAVAGNE <anthony.cavagne@ynov.com>
2021-09-30 21:59:24 +0200

    load reverse alphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -2,8 +2,8 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main()  {
-	for i := 122; i>= 97; i--{
+func main() {
+	for i := 122; i >= 97; i-- {
 		z01.PrintRune(rune(i))
 	}
 	z01.PrintRune('\n')

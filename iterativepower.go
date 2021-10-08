6605ecf00dd2ba3317eb1d3e59f08af0eadbbba3
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-08 09:50:45 +0200

    load ite

--- iterativepower.go
+++ iterativepower.go
@@ -2,9 +2,10 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	back := 1
-	if power <= 0 {
+	if power < 0 {
 		return 0
-	} else if power == 0 {
+	}
+	if power == 0 {
 		return 1
 	}
 	for i := 1; i <= power; i++ {

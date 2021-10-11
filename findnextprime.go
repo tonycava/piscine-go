16594d5b300a612969a4db6ea4c807e30a8803a5
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-11 21:49:43 +0200

    load lastrune

--- findnextprime.go
+++ findnextprime.go
@@ -1,10 +1,11 @@
 package piscine
 
 func FindNextPrime(nb int) int {
+
 	if nb <= 1 {
 		return 2
 	}
-	for i := 2; i < nb; i++ {
+	for i := 2; i < nb/2+1; i++ {
 		if nb%i == 0 {
 			return FindNextPrime(nb + 1)
 		}

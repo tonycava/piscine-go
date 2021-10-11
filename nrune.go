6e1144be0aabc4b4c890a8c67e59d17bb2dd2c65
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-11 16:16:51 +0200

    load lastrune

--- nrune.go
+++ nrune.go
@@ -2,9 +2,9 @@ package piscine
 
 func NRune(s string, n int) rune {
 	runes := []rune(s)
-	if n > len(runes) || n <= 0 {
+	if n > len(runes) || n <= 0 { // gestion d'erreur
 		return 0
-	} else {
+	} else { // dans le cas ou la gestion d'erreur est bonne
 		return runes[n-1]
 	}
 }

e73873b5414488465791e88448aa3b1f7708f784
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-11 21:12:32 +0200

    load lastrune

---
+++ findnextprime.go
@@ -0,0 +1,13 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb <= 0 {
+		return 0
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return FindNextPrime(nb + 1)
+		}
+	}
+	return nb
+}

ba98b0e3635799274c4efcdbfcb2396ab0659972
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-14 11:31:32 +0200

    load lastrune

---
+++ atoi.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Atoi(s string) int {
+	table := []rune(s)
+	art := 0
+	tale := len(table)
+	signe := 1
+	if table[0] == '-' {
+		signe = -1
+	}
+	for i := 0; i < tale; i++ {
+		if table[i] > '9' || table[i] < '0' && i != 0 {
+			return 0
+		} else {
+			art *= 10
+			art += int(table[i]) - '0'
+		}
+		if i == 0 && table[0] == '-' {
+			continue
+		}
+		if i == 0 && table[0] == '+' {
+			continue
+		}
+	}
+	return art * signe
+}

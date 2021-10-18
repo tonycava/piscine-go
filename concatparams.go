eab86832706a12b2faf59a7291b5032c7b6bbebb
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-18 14:48:33 +0200

    load lastrune

--- concatparams.go
+++ concatparams.go
@@ -3,7 +3,7 @@ package piscine
 func ConcatParams(args []string) string {
 	var tab string
 	for i := 0; i < len(args); i++ {
-		tab = tab + string(args[i]) + string("\n")
+		tab = tab + string(args[i]) + ("\n")
 	}
 	return tab
 }

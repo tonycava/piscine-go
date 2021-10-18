fab0ca4f443732d88db197430c4871437235d76a
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-18 15:15:47 +0200

    load lastrune

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,9 +2,8 @@ package piscine
 
 func SplitWhiteSpaces(s string) []string {
 	var tap []string
-	var temp string
 	for i := 0; i < len(s); i++ {
-		tap = append(tap, string(temp[i]))
+		tap = append(tap, string(s[i]))
 	}
 	return tap
 }

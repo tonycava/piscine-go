6cb807c3b47cc31b036df1a5dc70d04f8b693f12
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-18 14:11:17 +0200

    load lastrune

--- makerange.go
+++ makerange.go
@@ -4,11 +4,10 @@ func MakeRange(min, max int) []int {
 	var tab []int
 	if min >= max {
 		return nil
-	} else {
-		tale := make([]int, max-min)
-		for i := 0; i < (max - min); i++ {
-			tale[i] = min + i
-		}
+	}
+	tab = make([]int, max-min)
+	for i := 0; i < (max - min); i++ {
+		tab[i] = min + i
 	}
 	return tab
 }

33c38fe67f15298b65541992b8bc582b18a82350
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-19 11:37:20 +0200

    load lastrune

---
+++ point/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+	setPoint(points)
+	printStr("x = 42, y = 21")
+
+}

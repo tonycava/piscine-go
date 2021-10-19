7c32466f79a20020c60fd7b9db48ee43963464ca
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-19 14:15:37 +0200

    load lastrune

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	if len(os.Args) > 2 {
 		fmt.Println("Too many arguments")
+		return
 	}
 	if len(os.Args) < 2 {
 		fmt.Println("File name missing")
+		return
 	}
 	data, err := os.ReadFile(os.Args[1])
 	if err != nil {

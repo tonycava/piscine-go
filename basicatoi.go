6aeb13f94c09bbe10b7f3181581e413b60240710
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-20 09:10:28 +0200

    load lastrune

--- basicatoi.go
+++ basicatoi.go
@@ -6,7 +6,7 @@ func BasicAtoi(s string) int {
 	bb := len(table)
 	for i := 0; i < bb; i++ {
 		if table[i] > '9' || table[i] < '0' {
-			return art
+			return 0
 		} else {
 			art *= 10
 			art += int(table[i]) - '0'

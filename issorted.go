02591fa4bcfc457def2b9844ee96d9fbce4b8bde
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-22 10:18:24 +0200

    load lastrune

--- issorted.go
+++ issorted.go
@@ -1,9 +1,11 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i, nb := range a {
-		if f(nb+i, nb+i+1) > 0 {
-			return true
+	for j := 0; j < len(a); j++ {
+		for _, nb := range a {
+			if f(nb+j, nb+j+1) > 0 {
+				return true
+			}
 		}
 	}
 	return false

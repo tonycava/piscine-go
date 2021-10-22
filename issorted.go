41ec72318a92c9d99882294fe19003e608dcb195
ACAVAGNE <anthony.cavagne@ynov.com>
2021-10-22 10:37:57 +0200

    load lastrune

--- issorted.go
+++ issorted.go
@@ -4,11 +4,10 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	mybool := true
 	yourbool := true
 	for i := 1; i < len(a); i++ {
-			if f(a[i-1], a[i]) >= 0 {
-				mybool = false
-			} else {
-				yourbool = false
-			}
+		if f(a[i-1], a[i]) >= 0 {
+			mybool = false
+		} else {
+			yourbool = false
 		}
 	}
 	return mybool || yourbool
